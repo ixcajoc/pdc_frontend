f21860c552bc1a08fbd8ea39e1571cd0de4633f0
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2025-12-21 23:05:15 -0600

    add: company detail features

--- src/app/components/company/company-detail/company-detail.ts
+++ src/app/components/company/company-detail/company-detail.ts
@@ -4,6 +4,8 @@ import { Company, CompanyList,CompanyDetail } from '../../../interfaces/company.
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Banner } from "../../../shared/banner/banner";
+import Swal from 'sweetalert2';
+import { MessageService } from '../../../services/message-service.service';
 
 @Component({
   selector: 'app-company-detail',
@@ -18,6 +20,7 @@ export class CompanyDetailComponent {
   constructor(
     private router: Router,
     private companyService: CompanyService,
+    private messageService: MessageService,
 
   ){} 
     
@@ -41,5 +44,52 @@ export class CompanyDetailComponent {
     // const companyID = this.route.snapshot.params['id'];
     this.router.navigate([`dashboard1/update-company/${this.company.companyID}`]);
   }
+
+
+  confirmarEliminar(id: number, registro:string ) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      html: `Se eliminará <strong>${registro}</strong>.<br>Esta acción no se puede revertir.`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#dc2626',
+      cancelButtonColor: '#2563eb',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+      focusCancel: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.deleteCompany(id);
+      }
+    });
+  }
+
+  // Eliminar registro
+  deleteCompany(id: number) {
+    this.companyService.deleteCompany(id).subscribe({
+      next: (response) => {
+        console.log('Usuario eliminado:', response);
+              
+        Swal.fire({
+          title: '¡Eliminado!',
+          text: 'Usuario eliminado correctamente.',
+          icon: 'success',
+          timer: 2000,
+          showConfirmButton: false
+        });
+      },
+      error: (error) => {
+        console.error('Error al eliminar el usuario:', error);
+        const errorMessage = error.error?.message || 'Error desconocido al eliminar el registro';
+        Swal.fire({
+          title: 'Error',
+          text: errorMessage,
+          icon: 'error',
+          confirmButtonColor: '#3b82f6'
+        });
+      }
+    });
+  }
+  
 
 }
