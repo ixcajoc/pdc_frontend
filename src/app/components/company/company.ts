3765905f74fa35e68253d7e755ea097b9075b25e
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2025-12-21 23:40:22 -0600

    add: new-company features

--- src/app/components/company/company.ts
+++ src/app/components/company/company.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TableCompany } from './table-company/table-company';
 import { Company, CompanyList } from '../../interfaces/company.interface';
 import { Banner } from "../../shared/banner/banner";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-company',
   imports: [
     CommonModule,
     TableCompany,
-    Banner
+    Banner,
+    RouterLink
 ],
   templateUrl: './company.html',
   styleUrl: './company.css'
@@ -18,22 +20,30 @@ import { Banner } from "../../shared/banner/banner";
 export class CompanyComponent {
 
   constructor(
-      private companyService: CompanyService,
-    ) {}
+    private companyService: CompanyService,
+    private router: Router,
+
+  ) {}
   
-    ngOnInit():void{
-      this.getCompanies();
-    }
+  ngOnInit():void{
+    this.getCompanies();
+  }
+
+  companyList: Company[] = []
+
+  getCompanies(){
+    this.companyService.getAllCompanies().subscribe({
+      next:(response)=>{
+        this.companyList = response.data;
+        console.log(this.companyList);
+      },
+      error: (error) => (console.log(error))
+    });
+  }
+
+  goToNewCompany(){
+    this.router.navigate([`dashboard1/new-company`]);
+  }
+
   
-    companyList: Company[] = []
-  
-    getCompanies(){
-      this.companyService.getAllCompanies().subscribe({
-        next:(response)=>{
-          this.companyList = response.data;
-          console.log(this.companyList);
-        },
-        error: (error) => (console.log(error))
-      });
-    }
 }
--- src/app/components/company/new-company/new-company.ts
+++ src/app/components/company/new-company/new-company.ts
@@ -1,11 +1,86 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Banner } from '../../../shared/banner/banner';
+import { Company } from '../../../interfaces/company.interface';
+import { MessageService } from '../../../services/message-service.service';
+import { CompanyService } from '../../../services/company-service.service';
 
 @Component({
   selector: 'app-new-company',
-  imports: [],
+  imports: [
+     CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    Banner
+  ],
   templateUrl: './new-company.html',
   styleUrl: './new-company.css'
 })
 export class NewCompanyComponent {
+
+  
+  companyForm: FormGroup;
+  company!: Company;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private router: Router,
+    private companyService: CompanyService
+
+  ) 
+  {
+    this.companyForm = this.fb.group({
+      nit: ['', Validators.required],
+      businessName: ['', Validators.required],
+      commercialName: [''],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      country: ['', Validators.required],
+      department: ['', Validators.required],
+      municipality: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(){
+
+  }
+
+  newCompany(){
+    if (this.companyForm.invalid) {
+      this.companyForm.markAllAsTouched();
+      return;
+    }
+    let newCompany: Company = {
+      nit: this.companyForm.value.nit ?? '',
+      businessName: this.companyForm.value.businessName ?? '',
+      commercialName: this.companyForm.value.commercialName ?? '',
+      phone: this.companyForm.value.phone ?? '',
+      email: this.companyForm.value.email ?? '',
+      country: this.companyForm.value.country ?? '',
+      department: this.companyForm.value.department ?? '',
+      municipality: this.companyForm.value.municipality ?? ''
+    };
+
+    console.log(newCompany);
+
+    this.companyService.newCompany(newCompany).subscribe({
+      next: (response)=> {
+        this.messageService.successAlert()
+        this.router.navigate([`dashboard1/companies`]);
+      },
+      error: (error) => {
+        this.messageService.errorAlert(error)
+      }
+    });
+  }
+
+  cancelarNew(){
+      this.router.navigate([`dashboard1/companies`]);
+  }
+
 
 }
