730f33becd89ea1268e853f3c7e25f67e0ac4a86
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2025-12-22 04:37:53 -0600

    add: cambio de url al server deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,13 @@
 // local
-export const environment = {
-    production: false,
-    url: 'http://localhost:4000/api/',
-
-};
-
-// produccion
 // export const environment = {
-//     production: true,
-//     url: 'https://server-asofutbol.vercel.app/api/',
+//     production: false,
+//     url: 'http://localhost:4000/api/',
 
 // };
+
+// produccion
+export const environment = {
+    production: true,
+    url: 'https://pdc-backend-ar74.onrender.com/api/',
+
+};
